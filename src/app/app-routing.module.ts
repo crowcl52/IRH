87bbfe6fdd176092686f3db138ee05c475257eec
eduardo SANCHEZ <eduardo.sanchez@aori.mx>
2019-05-08 07:58:41 -0500

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { DocumentosComponent } from './documentos/documentos.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { ConvocatoriasComponent } from './convocatorias/convocatorias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'documentos', component: DocumentosComponent },
+  { path: 'eventos-y-capacitaciones', component: EventosComponent },
+  { path: 'convocatorias', component: ConvocatoriasComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { HistoriaComponent } from './historia/historia.component';
+import { DocumentosComponent } from './documentos/documentos.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { ConvocatoriasComponent } from './convocatorias/convocatorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ContactComponent,
+    HistoriaComponent,
+    DocumentosComponent,
+    EventosComponent,
+    ConvocatoriasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
